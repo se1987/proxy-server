d56e6d8ea5be6a64bdba01e8238bb1faf1fe0beb
se1987 <s.tsukuda1987@gmail.com>
2024-05-04 22:35:43 +0900

    CORS対策index.jsの修正

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const cors = require("cors")
 const app = express();
 
 const {createProxyMiddleware} = require("http-proxy-middleware");
@@ -14,14 +14,25 @@ const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 10000,
 });
-
+const whiteList = ["http://localhost:5173", "https://storied-kitten-b2870e.netlify.app/", "http://localhost:5030"];
+const corsOptions = {
+    origin: function(origin, callback){
+        if (whiteList.indexOf(origin) !== -1 || !origin){
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
+}
 app.use(limiter);
 
 app.get("/", (req, res) => {
     res.send("This is my proxy server")
 });
 
-app.use("/world-weather-data", (req, res, next) => {
+app.use("/world-weather-data", cors(corsOptions), (req, res, next) => {
     const city = url.parse(req.url).query
     createProxyMiddleware({
         target: `${process.env.BASE_API_URL_WORLD_WEATHER}${city}&api=no`,
